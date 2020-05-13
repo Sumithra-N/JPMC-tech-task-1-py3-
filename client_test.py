aa688a9510af94ffdd37a923eb4a14257c537b25
Sumithra N <sumithran63@gmail.com>
2020-05-13 07:11:54 +0000

    client_test.py edited online with Bitbucket

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint,getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +17,25 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote),(quote['stock'],quote['top_bid']['price'],quote['top_ask']['price'],(quote['top_bid']['price']+quote['top_ask']['price'])/2))
 
+    """------------- Add the unit test cases -------------"""
+  def test_getRatio_LessEqualThanB(self):
+    price_a=88.185
+    price_b=86.225
+    self.assertLessEqual(getRatio(price_a,price_b),price_b)
 
-  """ ------------ Add more unit tests ------------ """
+  def test_getRatio_PriceANotZero(self):
+    price_a=0
+    price_b=108.89
+    self.assertIsNotNone(getRatio(price_a,price_b),0)
 
+  def test_getRatio_PriceBZero(self):
+    price_a=87.65
+    price_b=0
+    self.assertIsNone(getRatio(price_a,price_b))
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
